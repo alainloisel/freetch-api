826ee6e83c97c96ee01fe90277d6a58859b8fd22
Alain <alainloisel@gmail.com>
2016-09-15 23:58:32 +0000

    sre

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -129,9 +129,9 @@ module NotificationHelper
 
     #ua = Urbanairship.push(notif)
     puts "publish" 
-    puts  "sweetch-#{user.id}"
+    puts  "sweetch-#{user.facebook_id}"
     puts message
-    pubnub = Publish.publish("sweetch-#{user.id}", { title: title, data: message[:data].merge(options)}) unless schedule_for
+    pubnub = Publish.publish("sweetch-#{user.facebook_id}", { title: title, data: message[:data].merge(options)}) unless schedule_for
     puts "publish end 2" 
     #ua,
     [pubnub]
